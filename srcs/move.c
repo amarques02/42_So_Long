ebb6745d0b2db7ee16308708f2dede34222a691d
Antonio thadeu Marques neto <amarques@c1r9s2.42lisboa.com>
2022-11-07 17:01:24 +0000

    Add check_move() function for improved code readability

--- srcs/move.c
+++ srcs/move.c
@@ -6,48 +6,71 @@
 /*   By: amarques <amarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 13:54:58 by amarques          #+#    #+#             */
-/*   Updated: 2022/10/24 16:40:55 by amarques         ###   ########.fr       */
+/*   Updated: 2022/11/07 16:27:45 by amarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/so_long.h"
 
-static void move(int x,int y, int keycode, t_mlx *data)
+static void	move(int x, int y, int keycode, t_mlx *data)
 {
-	if (data->map[(data->player_y /64) + (y /64)][(data->player_x / 64) + (x / 64)] == '1')
-		return ;
-	if (data->map[(data->player_y /64) + (y /64)][(data->player_x / 64) + (x / 64)] == 'E')
+	char		*str;
+	static int	steps;
+	int			i;
+
+	mlx_put_image_to_window(data->mlx, data->win, data->img[0], \
+	data->player_x, data->player_y);
+	data->player_x = x * 64;
+	data->player_y = y * 64;
+	mlx_put_image_to_window(data->mlx, data->win, data->img[\
+	(keycode == 100) * 2 + (keycode == 97) * 3 + (keycode == 119) * 4 + \
+	(keycode == 115) * 5], data->player_x, data->player_y);
+	steps++;
+	str = ft_itoa(steps);
+	i = -1;
+	while (++i < data->l)
 	{
-		if (data->collectable == 0)
-			exit_game(data);
-		return ;
+		mlx_put_image_to_window(data->mlx, data->win, data->img[21], \
+		i * 64, data->h * 64);
 	}
-	if (data->map[(data->player_y /64) + (y /64)][(data->player_x / 64) + (x / 64)] == 'C')
-	{
-		data->map[(data->player_y /64) + (y /64)][(data->player_x / 64) + (x / 64)] = '0';
-		data->collectable--;
-	}
-	mlx_put_image_to_window(data->mlx, data->win, data->img[0], data->player_x, data->player_y);
-	data->player_x += x;
-	data->player_y += y;
-	mlx_put_image_to_window(data->mlx, data->win, data->img[(keycode == 100) * 2 + (keycode == 97) * 3 \
-	+ (keycode == 119) * 4 + (keycode == 115) * 5], data->player_x, data->player_y);
+	mlx_string_put(data->mlx, data->win, 32, \
+	(map_height(data->map) * 64) + 32, 0xFFFFFF, str);
+	free(str);
 }
 
-int key(int keycode, void *param)
+static int	check_move(t_mlx *data, int x, int y)
 {
-	t_mlx	*data;
+	if (data->map[y][x] == '1')
+		return (0);
+	if (data->map[y][x] == 'E')
+	{
+		if (data->collectable == 0)
+			print_error(data, "You secured all the capys B-)", 0);
+		return (0);
+	}
+	if (data->map[y][x] == 'C')
+	{
+		data->collectable--;
+		data->map[y][x] = '0';
+	}
+	if (data->map[y][x] == 'X')
+		print_error(data, "Watch out! That capy didn\'t look too happy!", 0);
+	return (1);
+}
+
+int	key(int keycode, void *param, t_mlx *data)
+{
+	int		x;
+	int		y;
+
 	data = param;
-	ft_printf("%d\n", keycode);
-	int	x;
-	int	y;
-	
 	if (keycode == 65307)
-		exit_game(data);
+		print_error(data, "Game Closed", 0);
 	if (!(keycode == 100 || keycode == 97 || keycode == 115 || keycode == 119))
 		return (1);
-	x = ((keycode == 100) - (keycode == 97)) * 64;
-	y = ((keycode == 115) - (keycode == 119)) * 64;
-	move(x, y, keycode, data);
+	x = data->player_x / 64 + ((keycode == 100) - (keycode == 97));
+	y = data->player_y / 64 + ((keycode == 115) - (keycode == 119));
+	if (check_move(data, x, y))
+		move(x, y, keycode, data);
 	return (0);
 }
